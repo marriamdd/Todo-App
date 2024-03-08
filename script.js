24b6f2299af1a284263dbb122137770a8f4c1a18
marriamdd <davitashvili.m3@gmail.com>
2024-03-09 01:39:56 +0400

    add each todo task after submit both in html and local storage all thats currently in local storage is visible

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const list_item_text = document.querySelector(".list_item_text");
+const text_input = document.querySelector("#text_input_id");
+const div_for_lists=document.querySelector(".div_for_lists")
+let array=[]
+let unicid=0
+
+if(localStorage.getItem("todo")){
+    array=JSON.parse(localStorage.getItem("todo"))
+    array.forEach(task => {
+        unicid++;
+        div_for_lists.innerHTML+=`
+<div class="each_todo_div" data-todo-id=unicid >
+ <input type="checkbox" class="item_checkbox" id="item_checkbox">
+<p class="list_item_text">${task}</p>
+  <svg class="close_svg" xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+</div>
+
+` 
+
+    });
+
+    
+}
+
+text_input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter"&&text_input.value) {
+   
+
+    console.log(text_input.value);
+    array.push(text_input.value)
+
+    localStorage.setItem("todo",JSON.stringify(array));
+
+    unicid++;
+    div_for_lists.innerHTML+=`
+       <div class="each_todo_div" data-todo-id=unicid >
+        <input type="checkbox" class="item_checkbox" id="item_checkbox">
+       <p class="list_item_text">${text_input.value}</p>
+         <svg class="close_svg" xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+       </div>
+    
+       `
+       text_input.value = ""; 
+  }
+});
+
