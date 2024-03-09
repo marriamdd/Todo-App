c29e84d6396b1f0fb3643bf0f636a6c7f284f011
marriamdd <davitashvili.m3@gmail.com>
2024-03-09 20:03:27 +0400

    fix bugs

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const text_input = document.querySelector("#text_input_id");
 const div_for_lists = document.querySelector(".div_for_lists");
 let array = [];
 let unicid = 0;
-display();
 add_task();
+display();
 delete_task();
 
 function display() {
-  array = JSON.parse(localStorage.getItem("todo")) || [];
-  if (array.length > 1) {
+  if (localStorage.getItem("todo")) {
     array = JSON.parse(localStorage.getItem("todo"));
     array.forEach((task) => {
-      unicid++;
       div_for_lists.innerHTML += `
       <div class="each_todo_div" id=${unicid} >
        <input type="checkbox" class="item_checkbox" id="item_checkbox">
@@ -22,16 +20,6 @@ function display() {
       
       `;
     });
-  } else if (array > 0) {
-    unicid++;
-    div_for_lists.innerHTML += `
-    <div class="each_todo_div" id=${unicid} >
-     <input type="checkbox" class="item_checkbox" id="item_checkbox">
-    <p class="list_item_text">${localStorage.getItem("todo")}</p>
-      <svg class="close_svg" xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-    </div>
-    
-    `;
   }
 }
 
@@ -61,34 +49,20 @@ function add_task() {
 function delete_task() {
   const close = Array.from(document.querySelectorAll(".close_svg"));
   if (close) {
-    close.map((btn) => {
+    close.forEach((btn) => {
       btn.addEventListener("click", (event) => {
         const parent = event.target.closest(".each_todo_div");
         if (parent) {
           const parent_id = parent.id;
           console.log(parent_id);
 
-          if (array.length > 1) {
-            array = JSON.parse(localStorage.getItem("todo"))
-            console.log(array);
-            array.forEach((task, index) => {
-              if (index == parent_id) {
-                console.log(index);
-                console.log(task);
-                console.log(array);
-                array.splice(index);
-                console.log(array);
+          parent.remove();
 
-                localStorage.setItem("todo", array);
-                parent.remove();
-              }
-            });
-          } else {
-            parent.remove();
-            array.splice(0);
-            localStorage.removeItem("todo");
-            div_for_lists.innerHTML = "";
-          }
+          array = JSON.parse(localStorage.getItem("todo"));
+          array.splice(parent_id, 1);
+          localStorage.setItem("todo", JSON.stringify(array));
+
+          console.log(array);
         }
       });
     });
