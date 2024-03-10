4589b842fa22f41636d71da94f42f2f61ae7e3cd
marriamdd <davitashvili.m3@gmail.com>
2024-03-11 01:49:42 +0400

    add function for  complited tasks parent id in to localstorage

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const list_item_text = document.querySelector(".list_item_text");
 const text_input = document.querySelector("#text_input_id");
 const div_for_lists = document.querySelector(".div_for_lists");
 let array = [];
-let unicid = 0;
-
+let unicid= 0;
+let complited_id_array=[]
+const only_dones = [];
+// complited_into_localstorage()
 display();
-complited()
+complited();
 dark_mode();
 
 add_task();
@@ -13,13 +15,16 @@ clear_completed();
 delete_task();
 
 function display() {
+  complited();
   if (localStorage.getItem("todo")) {
     array = JSON.parse(localStorage.getItem("todo"));
 
-    array.forEach((task) => {
+    array.forEach((task,indx) => {
+        unicid++
+
       div_for_lists.innerHTML += `
      
-      <div  class="each_todo_div"  >
+      <div  class="each_todo_div" id=${indx}  >
    
       <div class="todo_text_X">
       <p class="list_item_text checked">${task}</p>
@@ -29,8 +34,7 @@ function display() {
       </div>
       
    
-      `;  
-      
+      `;
     });
 
     // console.log(array.length)
@@ -54,19 +58,7 @@ function add_task() {
 
       localStorage.setItem("todo", JSON.stringify(array));
 
-      unicid++;
-      div_for_lists.innerHTML += `
-     
-  
-       <div  class="each_todo_div" id=${unicid} >
-     
-       <div class="todo_text_X">
-       <p class="list_item_text">${text_input.value}</p>
-         <svg class="close_svg" xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-         </div>
-       </div>
     
-       `;
       text_input.value = "";
     }
 
@@ -93,6 +85,7 @@ function delete_task() {
           localStorage.setItem("todo", JSON.stringify(array));
 
           console.log(array);
+         
           location.reload();
         }
       });
@@ -109,16 +102,29 @@ function clear_completed() {
 }
 
 function complited() {
-    const task = Array.from(document.querySelectorAll(".each_todo_div"));
-  
-   task.forEach((btn) => {
-      btn.addEventListener("click", (event) => {
-        if (event.target.classList.contains("todo_text_X")) {
-          event.target.classList.toggle("complited_todo");
-        }
-      });
+  const task = Array.from(document.querySelectorAll(".each_todo_div"));
+
+  task.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      if (event.target.classList.contains("todo_text_X")) {
+        event.target.classList.toggle("complited_todo");
+       let complited_task= event.target
+        complited_into_localstorage(complited_task)
+      }
+    })
     });
-  }
+}
+
+function complited_into_localstorage(complited_task) {
+  const parent_id=complited_task.closest(".each_todo_div").id
+
+  complited_id_array.push(parent_id)
+  const storage=JSON.parse(localStorage.getItem("complited_id"))||[]
+
+  const setstorage=localStorage.setItem("complited_id",JSON.stringify(complited_id_array))
+
+  console.log(parent_id);
+}
 
 function dark_mode() {
   const moon_icon = document.querySelector(".moon_icon");
