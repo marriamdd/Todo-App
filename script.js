7e569196b4230a58f9886b5a0d3117b4a19c1000
marriamdd <davitashvili.m3@gmail.com>
2024-03-11 16:15:19 +0400

    fix display markeds after refresh and unmark process

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const list_item_text = document.querySelector(".list_item_text");
 const text_input = document.querySelector("#text_input_id");
 const div_for_lists = document.querySelector(".div_for_lists");
 let array = [];
-let unicid = 0;
-let completed_id_array=[];
-const only_dones = [];
-completed_into_localstorage()
+let completed_id_array = [];
+let classs = "";
+let completed= localStorage.getItem("completed_id")||[];
 display();
 complited();
 dark_mode();
@@ -16,19 +15,31 @@ delete_task();
 
 function display() {
   complited();
+
   if (localStorage.getItem("todo")) {
     array = JSON.parse(localStorage.getItem("todo"));
-
+    // let completed=JSON.parse( localStorage.getItem("completed_id"))||[];
+console.log(completed)
+console.log(array)
     array.forEach((task, indx) => {
-      unicid++;
+      console.log(indx);
+      console.log(completed)
+            if (completed.includes(indx)) {
+console.log("shhh")
+       classs="complited_todo"
 
+            }
+            else{
+                classs=""  
+            }
+           
       div_for_lists.innerHTML += `
      
       <div  class="each_todo_div" id=${indx}  >
    
-      <div class="todo_text_X">
+      <div class="todo_text_X ${classs}">
       <p class="list_item_text checked">${task}</p>
-        <svg class="close_svg" " xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+        <svg class="close_svg"  xmlns="http://www.w3.org/2000/svg"  width="14" height="14" viewBox="0 0 26 26" ><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
         </div>
         
       </div>
@@ -37,7 +48,6 @@ function display() {
       `;
     });
 
-    // console.log(array.length)
 
     div_for_lists.innerHTML += `
     <div class="list_last_line each_todo_div" >
@@ -50,7 +60,6 @@ function display() {
     }
   }
 }
-
 function add_task() {
   text_input.addEventListener("keyup", (event) => {
     if (event.key === "Enter" && text_input.value) {
@@ -59,6 +68,7 @@ function add_task() {
       localStorage.setItem("todo", JSON.stringify(array));
 
       text_input.value = "";
+     
     }
 
     if (event.key === "Enter") {
@@ -107,36 +117,43 @@ function complited() {
     btn.addEventListener("click", (event) => {
       if (event.target.classList.contains("todo_text_X")) {
         event.target.classList.toggle("complited_todo");
-        let completed_task= event.target;
-    //   
-        completed_into_localstorage(completed_task);
+        let completed_task = event.target;
+        let parent_id = completed_task.closest(".each_todo_div").id;
+        completed_into_localstorage(parent_id);
       }
     });
   });
 }
 
-function completed_into_localstorage(completed_task) {
+function completed_into_localstorage(parent_id) {
 
-    if(completed_task){
-        const parentID = completed_task.closest(".each_todo_div").id;
+    completed_id_array =JSON.parse(localStorage.getItem("completed_id")) || [];
+ 
 
-        if (!completed_id_array.includes(parentID)) {
-            completed_id_array.push(parentID);
-        } else {
-            const indexRemove = completed_id_array.indexOf(parentID);
-            if (indexRemove !== -1) {
-                completed_id_array.splice(indexRemove, 1);
-            }
-        }
-    
-        localStorage.setItem("completed_id", JSON.stringify(completed_id_array));
-      
-        console.log(completed_task);
-    }
+if (! completed_id_array.includes(parent_id)) {
+ 
+   completed_id_array.push(parent_id);
+
+
+  } else {
    
+    let index_for_delete = completed_id_array.indexOf(parent_id);
+    completed_id_array.splice(index_for_delete, 1);
+
+    console.log(index_for_delete);
+
+  }
+  
+  localStorage.setItem("completed_id", JSON.stringify(completed_id_array));
+  
+    // console.log(ls)
 }
 
 
+
+
+
+// completed_into_localstorage();
 function dark_mode() {
   const moon_icon = document.querySelector(".moon_icon");
   moon_icon.addEventListener("click", () => {
